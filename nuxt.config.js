9001dd31d6a7328b4920bbfc399c682bed79ed5b
djdiox <markuswagner93@gmail.com>
2022-12-04 15:00:53 +0100

    1 file modified

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '5-min-new',
+    title: "Markus Wagner's Homepage",
     htmlAttrs: {
       lang: 'en',
     },
